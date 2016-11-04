8616e15d4ee875f6ffa6b6650b5f8dd235970782
jingchaofang <jing@turingca.com>
2016-11-04 18:54:12 +0800

    udpate tool/sh

--- tool/init-fecs.sh
+++ tool/init-fecs.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
-cd ..
+
 git clone https://github.com/ireaderfe/ireader-fecs-githooks.git
 echo "拷贝文件...."
 
 root=$(cd `dirname $0`; pwd)
-gitPath=$root"/.git/"
+gitPath=$root"/../.git/"
 
 if [ ! -d "$gitPath" ]; then
     echo "非git仓库，退出"
 else
-    cd fecs-git-hooks
-    cp -f hooks/pre-commit ../.git/hooks/
-    cp -f .fecsrc ../
+    cd ireader-fecs-githooks
+    cp -f hooks/pre-commit $gitPath"hooks/"
+    #echo $gitPath"hooks/"
+    cp -f .fecsrc $gitPath"../"
+    #echo $gitPath"../"
     cd ..
     echo "pre-commit更新完成"
     echo ".fecsrc配置文件更新完成"
-    
 fi
 
 rm -rf ireader-fecs-githooks
